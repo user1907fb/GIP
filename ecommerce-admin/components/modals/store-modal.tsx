31f4e045065315b1d19a9307f8ee21366a4472a9
user1907fb <116819460+user1907fb@users.noreply.github.com>
2024-01-10 21:30:31 +0100

    admin dashboard klaar

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import * as z from "zod";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
@@ -38,10 +41,11 @@ export const StoreModal = () => {
       setLoading(true);
 
       const response = await axios.post('/api/stores', values);
-
-      console.log(response.data);
+      
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       console.log(error);
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
